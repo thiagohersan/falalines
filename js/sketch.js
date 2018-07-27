1679b8f6f5eaf6901ee1f30d05b8586599ca7c32
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-27 17:42:51 -0300

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,29 @@
+let pos = 0;
+let fendaWidth = 4;
+let word = "HELP";
+let fontSize = 400;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(100);
+  textSize(400);
+  textAlign(CENTER, CENTER);
+  while(textWidth(word) > windowWidth) {
+    fontSize -= 2;
+    textSize(fontSize);
+  }
+}
+
+function draw() {
+  pos = (pos + 10) % width;
+  background(0);
+  fill(255);
+  text(word, width / 2, height / 2);
+  fill(0);
+  rect(0, 0, pos, height);
+  rect(pos+fendaWidth, 0, width-pos, height);
+}
+
+function windowResized() {
+  setup();
+}
